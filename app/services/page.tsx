6362faf53f4dfa228e78c05364ab70ef7f988bf7
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-04-19 18:05:06 -0300

    feat services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { ServicesItems } from "@/utils/constants";
 
 export default function ServicesPage() {
-  return <div>ServicesPage</div>;
+  return (
+    <section className="pt-8 lg:pt-12">
+      <h1 className="font-black text-brand-color text-2xl font-inter text-center lg:text-start lg:text-3xl">
+        Serviços
+      </h1>
+
+      <div className="flex py-4 flex-col gap-8 lg:mt-12 lg:gap-14 lg:flex-row">
+        {ServicesItems.map((service) => (
+          <article
+            key={service.title}
+            className="flex flex-col text-center lg:text-start gap-4 border-transparent hover:border-brand-color border-2 rounded-lg p-4 animation-all"
+          >
+            <h2 className="font-black font-inter">{service.title}</h2>
+
+            <p className="font-extralight text-muted font-inter text-sm">
+              {service.description}
+            </p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -5,3 +5,26 @@ export const NavItems = [
   { label: "Sobre mim", href: paths.About() },
   { label: "Contatos", href: paths.Contacts() },
 ];
+
+export const ServicesItems = [
+  {
+    title: "Tatuagens Personalizadas",
+    description:
+      "Oferecemos a criação de tatuagens personalizadas, onde cada design é desenvolvido em colaboração direta com o cliente. A partir de uma ideia inicial, trabalhamos juntos para criar uma obra de arte única que reflete sua personalidade e sentimentos.",
+  },
+  {
+    title: "Cover-Up",
+    description:
+      "Especializamo-nos em transformar tatuagens antigas ou indesejadas em novas obras de arte. Se você tem uma tatuagem que não representa mais quem você é, nós podemos ajudá-lo a reformulá-la com um design criativo e original.",
+  },
+  {
+    title: "Restauração",
+    description:
+      "Com o tempo, algumas tatuagens podem desbotar ou perder a definição. Oferecemos serviços de restauração para revitalizar sua tatuagem antiga, realçando as cores e contornos para que ela volte a ter a aparência de quando foi feita pela primeira vez.",
+  },
+  {
+    title: "Henna",
+    description:
+      "Para aqueles que não estão prontos para um compromisso permanente ou desejam testar um design antes de tatuá-lo definitivamente, oferecemos tatuagens temporárias de alta qualidade que duram de algumas semanas a alguns meses.",
+  },
+];
