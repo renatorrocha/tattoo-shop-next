813cdd4440e76ac23585d5935ade8f3f23dec2b8
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-04-22 17:02:52 -0300

    fix contact image

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -6,13 +6,12 @@ import ContactImg from "/public/contact-img.png";
 
 export default function ContactsPage() {
   return (
-    <section className="py-8 lg:pt-12 h-full w-full flex flex-grow lg:gap-24 items-center">
-      <div className="text-center items-center lg:text-start flex flex-col justify-center lg:gap-12 gap-48 -mt-24 flex-grow">
+    <section className="flex flex-col lg:flex-row py-8 lg:pt-12 h-full w-full justify-center items-center">
+      <div className="flex flex-col justify-center items-center lg:items-start text-center lg:text-left lg:gap-12 gap-6 lg:flex-grow">
         <div className="space-y-4">
           <h1 className="font-inter text-2xl font-black">
             Entre em <span className="text-brand-color">Contato</span>
           </h1>
-
           <p className="font-light font-inter text-typography">
             Clique no botão abaixo e envie uma mensagem. Vamos conversar!
           </p>
@@ -21,9 +20,14 @@ export default function ContactsPage() {
         <Link
           target="_blank"
           href={WhatsAppLink}
-          className="items-center flex justify-center gap-8 border-b-2 py-4 w-[244px] border-b-muted rounded-lg bg-[#494949] hover:scale-90 animation-all"
+          className="flex justify-center items-center gap-8 border-b-2 py-4 w-[244px] border-b-muted rounded-lg bg-[#494949] hover:scale-90 transition-all"
         >
-          <Image src={WhatsAppLogo} alt="WhatsApp logo" className="size-8" />
+          <Image
+            src={WhatsAppLogo}
+            alt="WhatsApp logo"
+            width={24}
+            height={24}
+          />
           <div className="flex flex-col items-start">
             <p className="font-inter font-medium text-lg">Enviar WhatsApp</p>
             <p className="font-inter text-xs text-muted">(+55 11) 99711 1111</p>
@@ -31,11 +35,13 @@ export default function ContactsPage() {
         </Link>
       </div>
 
-      <Image
-        src={ContactImg}
-        alt=""
-        className="hidden lg:inline w-[757px] h-[504px]"
-      />
+      <div className="lg:w-1/2 flex justify-center">
+        <Image
+          src={ContactImg}
+          alt="Contact"
+          className="object-cover hidden lg:inline"
+        />
+      </div>
     </section>
   );
 }
