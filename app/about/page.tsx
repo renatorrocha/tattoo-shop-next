194fa997d0dc5dd24983620b683530fd94dfa2ac
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-04-22 15:46:33 -0300

    feat about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,22 +4,25 @@ import BodyPic from "/public/body-pic.png";
 
 export default function AboutPage() {
   return (
-    <section className="py-8 lg:pt-12 space-y-8">
-      <h1 className="text-center font-black text-2xl">
-        Prazer, sou <span className="text-brand-color">Sandro Luz</span>
-      </h1>
+    <section className="flex justify-between lg:gap-12 py-8 lg:pt-12 relative">
+      <div className="space-y-8">
+        <h1 className="text-start font-black text-2xl">
+          Prazer, sou <span className="text-brand-color">Sandro Luz</span>
+        </h1>
 
-      <div className="flex flex-col gap-4">
-        {AboutItems.map((item, index) => (
-          <p className="font-light font-inter" key={index}>
-            {item.text}
-          </p>
-        ))}
+        <div className="flex flex-col gap-4 text-start">
+          {AboutItems.map((item, index) => (
+            <p className="font-light font-inter" key={index}>
+              {item.text}
+            </p>
+          ))}
+        </div>
+
+        <p className="text-4xl font-island text-center">Sandro Luz</p>
       </div>
 
-      <p className="text-4xl font-island text-center">Sandro Luz</p>
-
-      <Image src={BodyPic} alt="" className=" object-contain" />
+      {/* TODO: adjust image */}
+      <Image src={BodyPic} alt="" className="h-full hidden lg:inline " />
     </section>
   );
 }
